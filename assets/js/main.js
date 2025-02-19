e009a9a19b9b53320926a055bc22450c40a8a2c1
razorext2 <razorext2@gmail.com>
2025-02-19 09:55:47 +0700

    complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById('start');
     const controllerOne = document.getElementById('controller-one');
     const controllerThree = document.getElementById('controller-three');
+    const controllerFour = document.getElementById('controller-four');
+    const controllerSix = document.getElementById('controller-six');
 
     // menetapkan animasi pada navbar
     window.addEventListener('scroll', function () {
@@ -14,9 +16,9 @@ document.addEventListener("DOMContentLoaded", function () {
         header.style.borderBottom = showBackground ? '1px #121212 solid' : '';
     });
 
-    // menenampilkan main jika start diklik
+    // menenampilkan first-page jika start diklik
     startButton.addEventListener('click', function () {
-        const contentContainer = document.getElementById('main');
+        const contentContainer = document.getElementById('first-page');
         const hero = document.getElementById('hero');
         const controllerTwo = document.getElementById('controller-two');
 
@@ -32,12 +34,35 @@ document.addEventListener("DOMContentLoaded", function () {
     controllerOne.addEventListener('click', function () {
         document.getElementById('hero').style.display = 'block';
         document.getElementById('hero').style.animation = 'fadeInStay 0.5s forwards';
-        document.getElementById('main').style.display = 'none';
+        document.getElementById('first-page').style.display = 'none';
     })
 
     controllerThree.addEventListener('click', function () {
-        document.getElementById('main').style.display = 'none';
-        document.querySelector('footer').style.display = 'flex';
-        document.querySelector('footer').style.animation = 'fadeInStay 0.5s forwards';
+        const controllerTwo = document.getElementById('controller-five');
+
+        document.getElementById('first-page').style.display = 'none';
+        document.getElementById('second-page').style.display = 'flex';
+        document.getElementById('second-page').style.animation = 'fadeInStay 0.5s forwards';
+        controllerTwo.style.animation = 'scaleUp 0.5s forwards';
+        controllerTwo.style.animationDelay = '100ms';
+    })
+
+    controllerFour.addEventListener('click', function () {
+        document.getElementById('first-page').style.display = 'flex';
+        document.getElementById('second-page').style.display = 'none';
+    })
+
+    controllerSix.addEventListener('click', function () {
+        document.getElementById('second-page').style.display = 'none';
+        document.getElementById('third-page').style.display = 'flex';
+        document.getElementById('to-top').style.display = 'block';
+    })
+
+    document.getElementById('to-top').addEventListener('click', function () {
+        document.getElementById('hero').style.display = 'block';
+        document.getElementById('hero').style.animation = 'fadeInStay 0.5s forwards';
+        document.getElementById('first-page').style.display = 'none';
+        document.getElementById('third-page').style.display = 'none';
+        document.getElementById('to-top').style.display = 'none';
     })
 });
